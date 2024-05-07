69549056ddd327640bd531175facefef60a1bd11
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-05-07 00:16:07 -0300

    7-5-24

---
+++ conversao_tipos.py
@@ -0,0 +1,33 @@
+#float para inteiro
+preco = 12.50
+preco = int(preco)
+print(preco)
+
+#inteiro para float
+preco2 = 150
+preco2 = float(preco2)
+print(preco2)
+
+#conversão por divisão
+print(preco2/2)
+
+#numero pra string
+peso= 140
+idade = 15
+
+print(str(peso))
+print(str(idade))
+
+#agora voce concatena os valores para serem exibidos
+texto = f"peso {peso} idade {idade}"
+print(texto)
+
+# string pra numero
+altura= "177"
+ano = "1978"
+
+print(float(altura))
+print(float(ano))
+
+#não é possivel a conversao de string pra float
+
---
+++ funcao_entra_saida.py
@@ -0,0 +1,5 @@
+# função input
+nome = input("qual o seu nome:")
+
+# função print
+print(f"olá {nome}")
