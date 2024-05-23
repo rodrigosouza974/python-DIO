309084ae6c4239fe6a6b183d16308048ca518c6c
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-05-22 22:43:27 -0300

    projeto plano de internet concluido

--- projeto_plano_internet.py
+++ projeto_plano_internet.py
@@ -14,28 +14,56 @@ Como entrada solicite o consumo médio mensal de dados em GB (float).
 Saída
 Retorne o plano ideal para o cliente de acordo com o consumo informado na entrada.
 '''
+consumo_medio_mensal = 0
+
+print("""
+========================================
+ Solicitação de Consumo de Internet 
+========================================
+
+Planos Oferecidos:
+      
+- Plano Essencial Fibra - 50Mbps:
+      
+     Recomendado para um consumo médio de até 10 GB.
+      
+- Plano Prata Fibra    - 100Mbps: 
+      
+      Recomendado para um consumo médio acima de 10 GB até 20 GB.
+      
+- Plano Premium Fibra  - 300Mbps: 
+      
+      Recomendado para um consumo médio acima de 20 GB.
+
+============================================================
+
+      Por favor, informe seu consumo médio de internet em GB:
+
+      """)
+# Solicita ao usuário que insira o consumo médio mensal de dados:
+# TODO: Crie uma Estrutura Condicional para verifica o consumo médio mensal 
+def obter_consumo_mensal():
+    while True:
+        try:
+            # Tenta converter o input para float
+            consumo_medio_mensal = float(input("Qual o seu consumo de internet por mês: "))
+            return consumo_medio_mensal
+        except ValueError:
+            # Se a conversão falhar, exibe uma mensagem de erro e pede novamente
+            print("Por favor, insira um número válido.")
+
+obter_consumo_mensal()
 
 # TODO: Crie uma Função: recomendar_plano para receber o consumo médio mensal:
-# função input
-consumo_medio_mensal = input("Qual o seu consumo de internet por mês:")
-
+def recomendar_plano(consumo_medio_mensal):
+    if consumo_medio_mensal < 10.0:
+        return " O Plano recomendado  é O Essencial Fibra - 50Mbps"
+    elif 10.0 < consumo_medio_mensal < 20.0:
+        return " O Plano recomendado  é O Plano Prata Fibra - 100Mbps"
+    else:
+        return " O Plano recomendado  é O Plano Premium Fibra  - 300Mbps"
     
-# TODO: Crie uma Estrutura Condicional para verifica o consumo médio mensal 
-consumo_medio_mensal = "sucesso" if saldo >= saque else "falha"
-
-consumo_medio_mensal = int(input("Informe uma opção: [1]sacar \n [2]Extrato"))
-if opcao == 1:
-    valor = float(input("Informe a quantia para o saque: "))
-
-elif opcao == 2:
-    print(' Exibindo o extrato...')
-else :
-    print('Opção Inválida')
-
-# TODO: Retorne o plano de internet adequado:
-    
-
-# Solicita ao usuário que insira o consumo médio mensal de dados:
-consumo = float(input())
 # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
-print(recomendar_plano(consumo))
+# TODO: Retorne o plano de internet adequado:
+resultado = recomendar_plano(consumo_medio_mensal)
+print(resultado)
